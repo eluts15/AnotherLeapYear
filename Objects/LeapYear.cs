97c03c4777d41cb7bad34910269c812911dc36a6
eluts <eluts15@gmai.com>
2017-05-31 01:07:45 -0700

    initial commit.

---
+++ Objects/LeapYear.cs
@@ -0,0 +1,13 @@
+namespace LeapYear
+{
+  public class LeapYear
+  {
+    public LeapYear
+    {
+      public bool IsLeapYearTrue(int year)
+      {
+        return false //For now just return false.
+      }
+    }
+  }
+}
